22688b738e89da6cebbe06be96964a1c90d8c1bc
rkorzen <korzeniewski@gmail.com>
2022-08-27 10:42:30 +0200

    card list view

--- cards/urls.py
+++ cards/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+from . import views
+
+
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="cards/base.html"), name="home"),  # /
+    # path("", TemplateView.as_view(template_name="cards/base.html"), name="home"),  # /
+    path("", views.card_list, name="card-list"),  # /
 ]
--- cards/views.py
+++ cards/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from .models import Card
+from django.views.generic import (
+    ListView,
+)
 
 # widok funkcyjny
 def card_list(request):
     cards = Card.objects.all()
-    return render(
-        request,
-        "cards/card_list.html",
-        {"card_list": cards}
-    )
+    return render(request, "cards/card_list.html", {"card_list": cards})
+
+
+# wido klasowy - analog dla powyższego:
+class CardListView(ListView):
+    model = Card
