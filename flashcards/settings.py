9ca9835deb86646d0132c916ed4f4a5da17694ce
rkorzen <korzeniewski@gmail.com>
2022-08-28 15:12:08 +0200

    automatic configuration with django-heroku

--- flashcards/settings.py
+++ flashcards/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-(ty42b!5dq0_%(qyo15$cz6%7=wokc)odaloi_f37s!4ddkr-6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -140,3 +140,9 @@ INTERNAL_IPS = [
     "127.0.0.1",
     # ...
 ]
+CSRF_COOKIE_SECURE = True
+
+
+import django_heroku
+
+django_heroku(locals())
